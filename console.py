fe5c1f457a9f248cf8e21854889e34200a9a5da9
TobiLight <jgetitdonefast@gmail.com>
2023-08-09 20:03:48 +0100

    Program that contains the entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# File: console.py
+# Author: Oluwatobiloba Light
+"""
+Entry point of the command interpreter
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+    
+    def do_EOF(self, line):
+        """Exit the program using EOF (Ctrl+D)"""
+        print()  # Print a newline before exiting
+        return True
+    
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+    
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+    
+    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
